a67d80dc47f0e90978c7c96c09fb4f0826ccbea5
daniel10vieira <danielvieiraalcantara10@gmail.com>
2023-06-05 21:49:54 -0300

    Initialize repository new module JavaScript-web.

---
+++ selecionando Elementos.js
@@ -0,0 +1,50 @@
+/*---------------- Selecionar Elementos -----------------*/
+
+//const element = document.getElementById("main-text")
+
+//const element = document.getElementsByClassName("paragraph")
+
+//const element = document.getElementsByTagName('h1')
+
+//const element = document.querySelector('p') //--> Sempre Retorna o primeiro que ele encontrar.
+
+//const element = document.querySelectorAll('p')
+
+// const element = document.querySelector('img') 
+
+/* const element = document.querySelector('h1') */
+
+/*---------------- Textos -----------------*/
+
+/* element.innerText = "Hello Word!"*/
+
+/* element.TextContent = "Hello Word!"*/
+
+/* element.innerHTML = "Hello Word" */
+
+/*---------------- CSS -----------------*/
+
+/* const element = document.querySelector('h1') */
+
+/* element.style.color = 'red'  */
+
+/* element.style.background = 'blue' */
+
+/*---------------- CSS - Eventos -----------------*/
+
+/* const element = document.querySelector('input')
+
+element.onkeypress = () => {
+    console.log('Pressionei uma tecla.')
+} */
+
+/*---------------- Propriedades Eventos -----------------*/
+
+/* const element = document.querySelector('input')
+
+element.onkeypress = (event) => {
+    console.log(event.target.value)
+} */
+
+/*---------------- Event Lisening -----------------*/
+
