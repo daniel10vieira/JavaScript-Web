2c1e7534f99daeaea3af07003c5d8b8393592b8f
daniel10vieira <danielvieiraalcantara10@gmail.com>
2023-07-10 21:30:05 -0300

    I was finished this project of currency conversor.

--- Projeto/script.js
+++ Projeto/script.js
@@ -8,6 +8,7 @@ function convertValues(){
 
   const dolarToday = 5.2
   const euroToday = 6.2
+  const libraToday = 6.18
 
 
   if(currencySelect.value == "dolar"){
@@ -21,6 +22,12 @@ function convertValues(){
       style: "currency",
       currency: "EUR"
     }).format(inputCurrencyValue / euroToday)
+  }
+  if(currencySelect.value == "libra"){
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+      style: "currency",
+      currency: "GBP"
+    }).format(inputCurrencyValue / libraToday)
   }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -40,6 +47,10 @@ function changeCurrency (){
   if(currencySelect.value == "euro"){
     currencyName.innerHTML = "Euro"
     currencyImage.src = "./assets/Euro.svg"
+  }
+  if(currencySelect.value == "libra"){
+    currencyName.innerHTML = "Libra esterlina"
+    currencyImage.src = "./assets/libra.svg"
   }
 
   convertValues()
